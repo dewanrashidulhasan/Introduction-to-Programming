beab8b608d00c553db4eedcc605f5e31bf1f0417
Dewan Rashidul Hasan <drhz671@gmail.com>
2025-10-04 23:44:52 +0600

    1

---
+++ Module_1/1.c
@@ -0,0 +1,30 @@
+// #include<stdio.h>
+// int main()
+// {
+//     printf("Hellooooooooooow\t");
+//     printf("wor\tld");
+//     // return 0;
+// }
+
+// #include<stdio.h>
+// int main()
+// {
+//     int num1 = 5;
+//     num1 = 10;
+//     float f = 4.5;
+//     char c = 'h';
+//     printf("%d\n", num1);
+//     printf("%.2f\n", f);
+//     printf("%c", c);
+//     return 0;
+// } 
+
+#include<stdio.h>
+#include<stdbool.h>
+int main()
+{
+    bool b = true;
+    bool c = false;
+    printf("%d\n", b);
+    printf("%d", c);
+}
