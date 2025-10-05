3f4b7ec9e0555f85e357eb3654e85b08abd7c8ec
Dewan Rashidul Hasan <drhz671@gmail.com>
2025-10-05 11:59:23 +0600

    2

--- Module_1/input.c
+++ Module_1/input.c
@@ -1,10 +1,26 @@
+// #include<stdio.h>
+// int main()
+// {
+//     int a;
+//     float f;
+//     char c;
+//     scanf("%d %f %c", &a, &f, &c);
+//     printf("%d \n%f \n%c", a, f, c);
+//     return 0;
+// }
+
+// #include<stdio.h>
+// int main ()
+// {
+//     long long int a = 1000000000000000000;
+//     double f = 1234.1234;
+//     printf("%lld \n%lf", a, f);
+//     return 0;
+// }
 #include<stdio.h>
 int main()
 {
-    int a;
-    float f;
-    char c;
-    scanf("%d %f %c", &a, &f, &c);
-    printf("%d \n%f \n%c", a, f, c);
+    int num = 100;
+    printf("Number is = %d", num);
     return 0;
 }
