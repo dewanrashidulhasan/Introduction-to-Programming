849342a7143aee819cd309c9452898f603fec8f3
Dewan Rashidul Hasan <drhz671@gmail.com>
2025-10-05 22:32:39 +0600

    1

---
+++ Module_2/arithmatic.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+    {
+        int a = 10;
+        int b = 5;
+        int sum = a+b;
+        int sub = a-b;
+        int mul = a
+        printf("Summation = %d\nSubtraction = %d", sum, sub);
+        return 0;
+    }
