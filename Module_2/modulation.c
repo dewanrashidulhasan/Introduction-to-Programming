60f14b071e55275ee09850fcbcc21031efcd1516
Dewan Rashidul Hasan <drhz671@gmail.com>
2025-10-06 13:21:37 +0600

    1

--- Module_2/modulation.c
+++ Module_2/modulation.c
@@ -1,9 +1,9 @@
-#include<stdio.h>
-int main()
-{
-    int a = 12;
-    int b =5;
-    int rem = a % b;
-    printf("%d",rem );
-    return 0;
-}
+// #include<stdio.h>
+// int main()
+// {
+//     int a = 12;
+//     int b =5;
+//     int rem = a % b;
+//     printf("%d",rem );
+//     return 0;
+// }
