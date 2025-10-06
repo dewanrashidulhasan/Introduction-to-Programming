40fc0250e5a573f61aa59c9d53aa6b90993a990b
Dewan Rashidul Hasan <drhz671@gmail.com>
2025-10-06 14:11:51 +0600

    1

--- Module_2/nested_if_else.c
+++ Module_2/nested_if_else.c
@@ -2,5 +2,23 @@
 int main()
 {
     int tk;
-    scanf("%d", tk);
+    scanf("%d", &tk);
+    if (tk>=5000)
+    {
+        printf("Cox's Bazar jabo.\n");
+        if (tk>=10000)
+        {
+            printf("Saintmartin Jabo.\n");
+        }
+        else
+        {
+            printf("Cox eii last gontobbo.\n");
+        }
+    }
+    else
+    {
+        printf("Kothao jabo na.\n");
+    }
+    
+    return 0;
 }
