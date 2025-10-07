126dd5939216d22d92b719fa5ec905138c4dacf0
Dewan Rashidul Hasan <drhz671@gmail.com>
2025-10-07 23:08:11 +0600

    1

---
+++ Module_3/for_loop.c
@@ -0,0 +1,20 @@
+// #include<stdio.h>
+// int main()
+// {
+//     for(int i=1; i<=10; i++)
+//     {
+//         printf("i am sorry\n");
+//     }
+//     return 0;
+// }
+
+
+// #include<stdio.h>
+// int main()
+// {
+//     for(int i=1; i<=10; i++)
+//     {
+//         printf("%d\n", i);
+//     }
+//     return 0;
+// }
