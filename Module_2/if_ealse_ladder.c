ef6824c6dd3fea59c99a583ebe57b64c4bce0395
Dewan Rashidul Hasan <drhz671@gmail.com>
2025-10-07 10:03:34 +0600

    h

--- Module_2/if_ealse_ladder.c
+++ Module_2/if_ealse_ladder.c
@@ -1,23 +1,53 @@
-#include<stdio.h>
-int main()
-{
-    int tk;
-    scanf("%d", &tk);
-    if (tk>=100)
-    {
-        printf("Burger Khabo.");
-    }
-    else if (tk>=50)
-    {
-        printf("Fuska Khabo.");
-    }
-    else if (tk>=20)
-    {
-        printf("Smoke Korbo.");
-    }
+// #include<stdio.h>
+// int main()
+// {
+//     int tk;
+//     scanf("%d", &tk);
+//     if (tk>=100)
+//     {
+//         printf("Burger Khabo.");
+//     }
+//     else if (tk>=50)
+//     {
+//         printf("Fuska Khabo.");
+//     }
+//     else if (tk>=20)
+//     {
+//         printf("Smoke Korbo.");
+//     }
     
-    else
-    {
-        printf("Pore ekdin Khabo.");
-    }
-}
+//     else
+//     {
+//         printf("Pore ekdin Khabo.");
+//     }
+// }
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a;
+//     scanf("%d", &a);
+//     if (a%2==0)
+//     {
+//         printf("even");
+//     }
+//     else
+//     {
+//         printf("odd");
+//     }
+    
+// }
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a;
+//     scanf("%d", &a);
+//     if (a>=0)
+//     {
+//         printf("positive");
+//     }
+//     else{
+//         printf("negative");
+//     }
+// }
