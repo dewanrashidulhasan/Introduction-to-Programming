a771c8907d080dd1c1c9851d9cd733463779cf70
Dewan Rashidul Hasan <drhz671@gmail.com>
2025-10-08 10:47:00 +0600

    8

--- Week_1/Module_3/continue.c
+++ Week_1/Module_3/continue.c
@@ -13,18 +13,18 @@
 // }
 
 
-#include<stdio.h>
-int main()
-{
-    int n;
-    scanf("%d", &n);
-    for (int i = 1; i <=n; i++)
-    {
-        if(i%2==1)
-        {
-            continue;
-        }
-        printf("%d\n",i);
-    }
+// #include<stdio.h>
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+//     for (int i = 1; i <=n; i++)
+//     {
+//         if(i%2==1)
+//         {
+//             continue;
+//         }
+//         printf("%d\n",i);
+//     }
     
-}
+// }
