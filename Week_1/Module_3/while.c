9d599b41ab58415764600a9839e687c56359cb3d
Dewan Rashidul Hasan <drhz671@gmail.com>
2025-10-08 11:20:17 +0600

    a

--- Week_1/Module_3/while.c
+++ Week_1/Module_3/while.c
@@ -1,11 +1,13 @@
-#include<stdio.h>
-int main()
-{
-    int i=1;
-    while (i<=10)
-    {
-        printf("%d\n", i);
-        i++;
-    }
+// #include<stdio.h>
+// int main()
+// {
+//     int i=1;
+//     while (i<=10)
+//     {
+//         printf("%d\n", i);
+//         i++;
+//     }
     
-}
+// }
+
+
