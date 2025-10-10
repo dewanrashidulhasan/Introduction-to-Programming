0841c4783256837259a508d47556accb883e7504
Dewan Rashidul Hasan <drhz671@gmail.com>
2025-10-10 11:39:37 +0600

    1

---
+++ Week_1/Module_3/practice.c
@@ -0,0 +1,64 @@
+// #include<stdio.h>
+// int main()
+// {
+//    int n;
+//    scanf("%d", &n);
+//    for (int i = 1; i <= n; i++)
+//    {
+//     if (i%5==0)
+//     {
+//         printf("%d Yes\n",i);
+//     }
+//     else
+//     {
+//         printf("%d No\n",i);
+//     }
+    
+//    }
+   
+//    return 0;
+// }
+
+// #include<stdio.h>
+// int main()
+// {
+//     for (int i = 99; i >=1; i--)
+//     {
+//         if (i%2!=0)
+//         {
+//             printf("%d\n",i);
+//         }
+//     }
+    
+// }
+
+// #include <stdio.h>
+
+// int main()
+// {
+// printf("Hello, world! I am learning C programming language. ^_^\n");
+// printf("Programming is fun and challenging. /\\/\\/\\\n");
+// printf("I want to give my 100%% dedication to learn!\tI will succeed one day.\n");
+// }
+
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     scanf("%d", &n);
+//     if (n>1000)
+//     {
+//         printf("I will buy Punjabi\n");
+//         if (n>=1500)
+//         {
+//             printf("I will buy new shoes\n");
+//             printf("Alisa will buy new shoes\n");
+//         }  
+//     }
+//     else
+//     {
+//         printf("Bad luck!");
+//     }
+    
+    
+//     return 0;
+// }
