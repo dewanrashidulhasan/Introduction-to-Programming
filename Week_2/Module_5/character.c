f8dbf14dfd4da9a48c66525e4060d287d90d603b
Dewan Rashidul Hasan <drhz671@gmail.com>
2025-10-12 00:56:25 +0600

    x

---
+++ Week_2/Module_5/character.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{   char x;
+    scanf("%c", &x);
+    if (x>='a' && x<='z')
+    {
+        x=x-32;
+        printf("%c",x);
+    }
+    else
+    {
+        x=x+32;
+        printf("%c",x);
+    }
+    
+    
+    return 0;
+}
