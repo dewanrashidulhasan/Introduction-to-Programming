d180fe8dad0e80ee5934fd5376f71a7e59ecb002
Dewan Rashidul Hasan <drhz671@gmail.com>
2025-10-13 00:46:22 +0600

    x

---
+++ Week_2/Module_5/max_and_min.c
@@ -0,0 +1,97 @@
+// #include<stdio.h>
+// int main()
+// {   int a,b,c;
+//     scanf("%d %d %d", &a,&b,&c);
+//     // min-
+//     if (a<b && a<c)
+//     {
+//         printf("%d ",a);
+//     }
+//     else if (b<=a && b<=c)
+//     {
+//         printf("%d ",b);
+//     }
+//     else if (c<=a && c<=b)
+//     {
+//         printf("%d ",c);
+//     }
+
+//     // max-
+//     if (a>=b && a>=c)
+//     {
+//         printf("%d",a);
+//     }
+//     else if (b>=a && b>=c)
+//     {
+//         printf("%d",b);
+//     }
+//     else if (c>=a && c>=b)
+//     {
+//         printf("%d",c);
+//     }
+//     return 0;
+// }
+
+// #include<stdio.h>
+// int main()
+// {   int a,b,c;
+//     scanf("%d %d %d",&a,&b,&c);
+//     if (a>b&&b>c)
+//     {
+//         printf("%d",a);
+//     }
+//     else if (b>a &&c>b)
+//     {
+//         printf("%d",b);
+//     }
+//     else{
+//         printf("%d",c);
+//     }
+    
+    
+//     return 0;
+// }
+
+// #include<stdio.h>
+// int main()
+// {   int x;
+//     scanf("%d",&x);
+//     if (x>=50)
+//     {
+//         printf("hello");
+//     }
+//     else if (x>=40)
+//     {
+//         printf("hii");
+//     }
+//     else if (x>=40)
+//     {
+//         printf("hi");
+//     }
+    
+    
+    
+//     return 0;
+// }
+
+// #include<stdio.h>
+// int main()
+// {   int x = 5;
+// int y = 4;
+// if(x>=y && x<y+4)
+// {
+// if(y<5)
+// {
+// printf("hi ");
+// }
+// else if(y<10)
+// {
+// printf("hi ");
+// }
+// }
+// else
+// {
+// printf("hello ");
+// }
+//     return 0;
+// }
