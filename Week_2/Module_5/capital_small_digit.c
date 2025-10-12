01d087538f83e0cf90e1b54ffd19a63ff90a63de
Dewan Rashidul Hasan <drhz671@gmail.com>
2025-10-12 10:12:48 +0600

    a

---
+++ Week_2/Module_5/capital_small_digit.c
@@ -0,0 +1,25 @@
+// #include<stdio.h>
+// int main()
+// {   char ch;
+//     scanf("%c", &ch);
+//     if (ch>='0' && ch<='9')
+//     {
+//         printf("IS DIGIT");
+//     }
+//     else if (ch>='a' && ch<='z' || ch>='A' && ch<='Z')
+//     {
+//         printf("ALPHA\n");
+//         if (ch>='A' && ch<='Z')
+//         {
+//             printf("IS CAPITAL");
+//         }
+//         else
+//         {
+//             printf("IS SMALL");
+//         }
+        
+//     }
+    
+    
+//     return 0;
+// }
--- Week_2/Module_5/welcome_with_condition.c
+++ Week_2/Module_5/welcome_with_condition.c
@@ -13,3 +13,34 @@
     
 //     return 0;
 // }
+// #include<stdio.h>
+// int main()
+// {   int x;
+//     scanf("%d",&x);
+//     int first_digit = x/1000;
+//     if (first_digit%2==0)
+//     {
+//         printf("Even");
+//     }
+//     else
+//     {
+//         printf("Odd");
+//     }
+//     return 0;
+// }
+
+// #include<stdio.h>
+// int main()
+// {   int x;
+//     scanf("%d",&x);
+//     int last_digit = x%10;
+//     if (last_digit%2==0)
+//     {
+//         printf("Even");
+//     }
+//     else
+//     {
+//         printf("Odd");
+//     }
+//     return 0;
+// }
