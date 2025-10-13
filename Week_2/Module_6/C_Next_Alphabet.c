5562f71c73bb2898c55ed858829a69bab7ab4184
Dewan Rashidul Hasan <drhz671@gmail.com>
2025-10-14 02:26:46 +0600

    1

---
+++ Week_2/Module_6/C_Next_Alphabet.c
@@ -0,0 +1,22 @@
+// #include<stdio.h>
+// int main()
+// {   char ch;
+//     scanf("%c",&ch);
+    
+//     if (ch=='z')
+//     {
+//         printf("%c",'a');
+//     }
+//     else if (ch=='Z')
+//     {
+//         printf("%c",'A');
+//     }
+//     else
+//     {
+//         printf("%c",ch+1);
+//     }
+    
+    
+    
+//     return 0;
+// }
