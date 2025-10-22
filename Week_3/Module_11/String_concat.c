5162c71d2dc61cdcff967c6c6131fa4df294a905
Dewan Rashidul Hasan <drhz671@gmail.com>
2025-10-22 10:42:29 +0600

    w

--- Week_3/Module_11/String_concat.c
+++ Week_3/Module_11/String_concat.c
@@ -1,15 +1,28 @@
+// #include<stdio.h>
+// #include<string.h>
+// int main()
+// {   char a[101], b[101];
+//     scanf("%s %s",&a,&b);
+//     int length =strlen(a);
+//     int lengths =strlen(b);
+//     for (int  i = 0; i <=lengths; i++)
+//     {
+//         a[i+length]=b[i];
+//     }
+    
+//     printf("%s %s",a,b);
+//     return 0;
+// }
+
+
+
+
 #include<stdio.h>
 #include<string.h>
 int main()
 {   char a[101], b[101];
     scanf("%s %s",&a,&b);
-    int length =strlen(a);
-    int lengths =strlen(b);
-    for (int  i = 0; i <=lengths; i++)
-    {
-        a[i+length]=b[i];
-    }
-    
+    strcat(a,b);
     printf("%s %s",a,b);
     return 0;
 }
