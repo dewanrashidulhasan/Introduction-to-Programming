cf39d1471206f0e204b011754eb7bbd685fbc999
Dewan Rashidul Hasan <drhz671@gmail.com>
2025-10-24 12:33:05 +0600

    1

--- Week_3/Module_11/J_Count_Letters.c
+++ Week_3/Module_11/J_Count_Letters.c
@@ -1,13 +1,16 @@
 #include<stdio.h>
 #include<string.h>
 int main()
-{   char a[1000001];
-    scanf("%s",&a);
+{   char a[10000005];
+    scanf("%s",a);
     int frequency[26] = {0}; 
     for (int  i = 0; a[i]!='\0'; i++)
     {
+        if (a[i]>='a'&& a[i]<='z')
+        {
         int val = a[i]-'a';
         frequency[val]++;
+        }    
     }
     for (int  i = 0; i <26; i++)
     {
@@ -20,3 +23,4 @@ int main()
     }
     return 0;
 }
+//https://docs.google.com/document/d/1FLggcwY4YLUkk6RpOWnePSCXBq0iZZdo4tsq6eUXz5s/edit?tab=t.0
