62f7bbbcad7cb5cdf8da7a26424cc7bfddba00d7
Dewan Rashidul Hasan <drhz671@gmail.com>
2025-11-07 19:35:11 +0600

    2

--- Week_4/Assignment_3/Is_Palindrome.c
+++ Week_4/Assignment_3/Is_Palindrome.c
@@ -1,11 +1,31 @@
 #include<stdio.h>
-int is_palindrome(s)
+#include<string.h>
+int is_palindrome(char s[])
 {
-    
+    int length= strlen(s);
+    int is_palindrom=1;
+    for (int i = 0,j=length-1; i <j; i++,j--)
+    {
+        if (s[i]!=s[j])
+        {
+            is_palindrom=0;
+            break;
+        }
+        
+    }
+    return is_palindrom;
 }
 int main()
 {   char s[1001];
     scanf("%s",s);
-
+    if (is_palindrome(s))
+    {
+        printf("Palindrome");
+    }
+    else
+    {
+        printf("Not Palindrome");
+    }
+    
     return 0;
 }
