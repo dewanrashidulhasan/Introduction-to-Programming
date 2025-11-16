40b027227f7103daffdc6bed867dda6a7f914bf8
Dewan Rashidul Hasan <drhz671@gmail.com>
2025-11-16 11:17:42 +0600

    1

---
+++ c++/Week_1/Module_1/input.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    char c;
+    double d;
+    cin>>x>>c>>d;
+    cout<<x<<" "<<c<<endl<<d<<
+    endl<<(int)c<<
+    endl<<(char)x<<endl;
+    return 0;
+}
---
+++ c++/Week_1/Module_1/print.cpp
@@ -0,0 +1,16 @@
+// #include<iostream>
+// int main()
+// {
+//     std::cout<<"heoll world";
+//     return 0;
+// }
+
+#include<iostream>
+int main()
+{
+    int x=10;
+    char c='A';
+    double d=34.56;
+    std:: cout <<x<<" "<<c<<std::endl<<d<<std::endl;
+    return 0;
+}
