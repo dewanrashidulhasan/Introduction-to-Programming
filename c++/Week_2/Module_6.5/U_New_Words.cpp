56689574cfcb6490eb31717311f5a43a9064591c
Dewan Rashidul Hasan <drhz671@gmail.com>
2025-11-29 02:38:01 +0600

    1

---
+++ c++/Week_2/Module_6.5/U_New_Words.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    getline(cin,s);
+    for (auto i=s.begin(); i < s.end(); i++)
+    {
+        cout<<*i<<endl;
+    }
+    return 0;
+}
