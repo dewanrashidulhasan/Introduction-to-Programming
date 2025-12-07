29703c0bb9e26bbb0e8bf25024d4c8f381513b65
Dewan Rashidul Hasan <drhz671@gmail.com>
2025-12-07 15:54:36 +0600

    1

---
+++ c++/Problem_solving_week_1/Minimum_Cars_required.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int n;
+        cin>>n;
+        double d= (double)n/4;
+        cout<<ceil(d)<<endl;
+        
+        
+    }
+    
+}
