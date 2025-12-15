4eb8fa339e927af8176dd3d38f4de4ddc3036fa9
Dewan Rashidul Hasan <drhz671@gmail.com>
2025-12-16 00:40:56 +0600

    1

---
+++ Basic Data Structures/Week_1/Module_2/vector_capacity.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // vector<int> v;
+    // cout<<v.capacity()<<endl;
+    // v.push_back(10);
+    // cout<<v.capacity()<<endl;
+    // v.push_back(10);
+    // cout<<v.capacity()<<endl;
+
+    vector<int> v;
+    v.push_back(10);
+    v.push_back(20);
+    v.push_back(30);
+    cout<<v.size()<<endl;
+    // v.clear();
+    // cout<<v.size()<<endl;
+    // cout<<v[0]<<endl;
+    // v.resize(6,100);
+    for (int i = 0; i <v.size(); i++)
+    {
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+    v.pop_back();
+    for (int i = 0; i <v.size(); i++)
+    {
+        cout<<v[i]<<" ";
+    }
+    return 0;
+}
---
+++ Basic Data Structures/Week_1/Module_2/vector_modifiers.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int> v= {1,2,3,2,5,2};
+    
+    // vector<int> v2;
+    // v2=v;
+    // for (int i = 0; i < v.size(); i++)
+    // {
+    //     cout<<v2[i]<<" ";
+    // }
+    //index neah kaj na korle
+    //necher sortcut forloop use 
+    //korte pare
+    // for(int x:v)
+    // {
+    //     cout<<x<<" ";
+    // }
+    // v.insert(v.begin()+2,100);
+
+    // vector<int> v2= {6,7,8,9,10};
+    // v.insert(v.begin()+5,v2.begin(),v2.end());
+    // for(int x:v)
+    // {
+    //     cout<<x<<" ";
+    // }
+    // cout<<endl;
+    // v.erase(v.begin()+1,v.begin()+5);
+    // for(int x:v)
+    // {
+    //     cout<<x<<" ";
+    // }
+
+    // replace(v.begin(),v.end()-1,2,100);
+    // for(int x:v)
+    // {
+    //     cout<<x<<" ";
+    // }
+    auto it=find(v.begin(),v.end(),2);
+    cout<<*it<<endl;
+    
+    return 0;
+}
