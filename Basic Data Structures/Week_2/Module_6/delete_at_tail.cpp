7a1ed61de08cd23d068c74507c66178d5a84b56c
Dewan Rashidul Hasan <drhz671@gmail.com>
2025-12-24 11:54:31 +0600

    1

---
+++ Basic Data Structures/Week_2/Module_6/delete_at_tail.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+    public:
+    int val;
+    Node* next; 
+    Node(int val)
+    {
+        this->val=val;
+        this->next=NULL;
+    }
+    
+};
+void insert_at_tail(Node* &head,Node* &tail,int val)
+{
+    Node* newnode=new Node(val);
+    if (head==NULL)
+    {
+        head=newnode;
+        tail=newnode;
+        return;
+    }
+    
+   
+    tail->next=newnode;
+    tail=newnode;
+}
+void print_linked_list(Node*  &head)
+{
+    Node* temp=head;
+    while (temp!=NULL)
+    {
+        cout<<temp->val<<endl;
+        temp=temp->next;
+    }
+    
+}
+void delete_at_tail(Node* &head,Node* &tail,int indx)
+{
+    Node* temp=head;
+    for (int i = 1; i < indx; i++)
+    {
+        temp=temp->next;
+    }
+    Node* deleNode=temp->next;
+    temp->next=temp->next->next;
+    delete deleNode;
+    tail=temp;
+}
+int main()
+{
+    Node* head=NULL;
+    Node* tail=NULL;
+    int val;
+    while (true)
+    {
+        cin>>val;
+        if (val==-1)
+        {
+            break;
+        }
+        insert_at_tail(head,tail,val);
+    }
+    int indx;
+    delete_at_tail(head,tail,3);
+    print_linked_list(head);
+    return 0;
+}
