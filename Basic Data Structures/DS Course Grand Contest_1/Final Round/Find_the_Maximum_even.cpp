6bceaea9278a68b5943cfbfb333ae49304695dcd
Dewan Rashidul Hasan <drhz671@gmail.com>
2026-02-11 19:12:51 +0600

    1

--- Basic Data Structures/DS Course Grand Contest_1/Final Round/Find_the_Maximum_even.cpp
+++ Basic Data Structures/DS Course Grand Contest_1/Final Round/Find_the_Maximum_even.cpp
@@ -9,53 +9,23 @@ int main()
     {
         cin>>a[i];
     }
-    long long big_even=LLONG_MIN,even1=LLONG_MIN,even2=LLONG_MIN,odd1=LLONG_MIN,odd2=LLONG_MIN;
+    long long mx=0;
     for (int i = 0; i < n; i++)
     {
         if (a[i]%2==0)
-        {
-            big_even=max(big_even,a[i]);
-            if (a[i]>even1)
-            {
-                even2=even1;
-                even1=a[i];
-            }
-            else if (a[i]>even2)
-            {
-                even2=a[i];
-            }
+            mx=a[i];
             
-        }
-        else
+    }
+    long long mx2=0;
+    for (int i = 0; i < n-1; i++)
+    {
+        for (int j = i+1;j < n; j++)
         {
-            if (a[i]>odd1)
-            {
-                odd2=odd1;
-                odd1=a[i];
-            }
-            else if (a[i]>odd2)
-            {
-                odd2=a[i];
-            }
+            long long sum=a[i]+a[j];
+            if(sum%2==0&&mx<sum)
+                mx2=sum;
         }
-        
     }
-    long long ans= big_even;
-    if (even1!=-1&&even2!=-1)
-    {
-        ans=max(ans,even1+even2);
-    }
-    
-    if (odd1!=-1&&odd2!=-1)
-    {
-        ans=max(ans,odd1+odd2);
-    }
-    if (ans==LLONG_MIN)
-    {
-        cout<<"-1"<<endl;
-    }
-    else
-    cout<<ans<<endl;
-    
+    cout<<max(mx,mx2)<<endl;
     return 0;
 }
