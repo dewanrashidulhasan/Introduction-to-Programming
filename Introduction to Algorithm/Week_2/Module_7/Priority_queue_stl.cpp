f5b3df2fea010eeb1b258520c49739d3076cd9fb
Dewan Rashidul Hasan <drhz671@gmail.com>
2026-02-22 00:04:05 +0600

    1

--- Introduction to Algorithm/Week_2/Module_7/Priority_queue_stl.cpp
+++ Introduction to Algorithm/Week_2/Module_7/Priority_queue_stl.cpp
@@ -3,10 +3,15 @@ using namespace std;
 int main()
 {
     // priority_queue<int> pq;//max priority queue
-    priority_queue<int,vector<int>,greater<int>> pq;//min priority queue
-    pq.push(10);
-    pq.push(20);
-    pq.push(30);
-    cout<<pq.top()<<endl;
+    // priority_queue<int,vector<int>,greater<int>> pq;//min priority queue
+    // pq.push(10);
+    // pq.push(20);
+    // pq.push(30);
+    // cout<<pq.top()<<endl;
+
+    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;//min priority queue
+    pq.push({10,1});
+    pq.push({5,2});
+    cout<<pq.top().first<<" "<<pq.top().second<<endl;
     return 0;
 }
