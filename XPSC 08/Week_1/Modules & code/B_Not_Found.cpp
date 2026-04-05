45712e8d31c58e2880d0841e6ce758b89b56f7c9
Dewan Rashidul Hasan <drhz671@gmail.com>
2026-04-05 20:34:59 +0600

    1

---
+++ XPSC 08/Week_1/Modules & code/B_Not_Found.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+    int freq[26]={0};
+    for (int i = 0; i < s.size(); i++)
+    {
+        freq[s[i]-'a']++;
+    }
+    for (int i = 0; i < 26; i++)
+    {
+        if (freq[i]==0)
+        {
+            cout<<char('a'+i)<<"\n";
+            return 0;
+        }
+    }
+    if(true)
+        cout<<"None"<<"\n";
+    
+    return 0;
+}
