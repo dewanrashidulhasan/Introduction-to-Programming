032a3a136a0c0b83d5679d3c9aa13c49638d0036
Dewan Rashidul Hasan <drhz671@gmail.com>
2026-07-04 12:34:47 +0600

    2

---
+++ XPSC 08/Week_7/Modules & code/Dull_Operation.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        ll n;
+        cin>>n;
+        ll x=n,y=n-1;
+        cout<<x<<" "<<y<<'\n';
+    }
+    
+    
+    return 0;
+}
